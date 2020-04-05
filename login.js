a3a2fbeaf8957c7865236f65d34c16ef2c84d1ef
Mihir_Khare429 <42797667+Mihir-Khare429@users.noreply.github.com>
2020-04-05 23:56:47 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const app=express()
+const bcrypt=require('bcrypt')
+const users=[]
+app.set('view-engine','ejs')
+app.use(express.urlencoded({urlencoded: false}))
+app.get('/',function(req,res){
+    res.render('index.ejs')
+})
+app.get('/login',function(req,res){
+    res.render('login.ejs')
+})
+app.post('/login',function(req,res){
+
+})
+app.get('/register',function(req,res){
+    res.render('register.ejs')
+})
+app.post('/register',async function(Req,res){
+    try{
+        const hashedpasswrd= await bcrypt.hash(req.body.password,10)
+        users.push({
+            id:Date.now().toString(),
+            name:req.body.name,
+            email:req.body.email,
+            password: hashedpasswrd
+        })
+        res.redirect('/login')
+    }catch{
+        console.log(users)
+    }
+})
+app.listen(3000,function(){
+    console.log('Listening at port 3000....!')
+})
